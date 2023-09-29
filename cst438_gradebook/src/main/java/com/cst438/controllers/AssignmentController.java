a073687996d711e4e44118c062dbdd815618413e
Dylan Masters <dmasters@csumb.edu>
2023-09-29 12:37:14 -0700

    updated CRUD methods

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -55,42 +55,74 @@ public class AssignmentController {
 	// TODO create CRUD methods for Assignment
 	@GetMapping("/assignment/{id}")
 	public AssignmentDTO getAssignment(@PathVariable("id") int id)  {
-		return null;
 		// check that assignment belongs to the instructor
-      // return Assignment data for the given assignment 
-      // if assignment not found, return HTTP status code 404
+		AssignmentDTO[] assignments = getAllAssignmentsForInstructor();
+		AssignmentDTO assignment = null;
+		for(int i = 0; i < assignments.length; i++) {
+			if(assignments[i].id() == id) {
+				assignment = assignments[i];
+			}
+		}
+	    // return Assignment data for the given assignment 
+	    // if assignment not found, return HTTP status code 404
+		if(assignment == null) {
+			return null;
+		}else {
+			return assignment;
+		}
 	}
-	
 	
 	@PostMapping("/assignment")
 	public int createAssignment(@RequestBody AssignmentDTO adto) {
-		return 0;
 		// check that course id in AssignmentDTO exists and belongs 
-      //  to this instructor
-	// then create a new Assignment Entity and save to database
-	// return the assignment id of the new assignment
+        //  to this instructor
+		AssignmentDTO[] assignments = getAllAssignmentsForInstructor();
+		for(int i = 0; i < assignments.length; i++) {
+			if(assignments[i].id() == adto.id() ) {
+				return 0;
+			}
+		}
+		// then create a new Assignment Entity and save to database
+		assignments[assignments.length] = adto;
+		//setAssignments(assignments);
+		// return the assignment id of the new assignment
+		return(adto.id());
 	}
-	
 	
 	@PutMapping("/assignment/{id}")
 	public void updateAssignment(
 	      @PathVariable("id") int id, 
 	      @RequestBody AssignmentDTO adto) {
 		// check assignment belongs to a course for this instructor
-	// update assignment with data in AssignmentDTO
+		AssignmentDTO[] assignments = getAllAssignmentsForInstructor();
+		for(int i = 0; i < assignments.length; i++){
+			if(assignments[i].id() == id) {
+				// update assignment with data in AssignmentDTO
+				assignments[i] = adto;
+			}
+		}
+		return;
 	}
-	
-
-
-	
+		
 	@DeleteMapping("/assignment/{id}")
 	public void deleteAssignment(
 	       @PathVariable("id") int id, 
 	       @RequestParam("force") Optional<String> force) {
 		// check assignment belongs to this instructor
-	// delete assignment if there are no grades.
-	// if there are grades for the assignment, delete the 
-	// assignment only if "force" is specified
+		AssignmentDTO[] assignments = getAllAssignmentsForInstructor();
+		for( int i = 0; i < assignments.length; i++) {
+			if(assignments[i].id() == id) {
+				for(int k = i; k < assignments.length-1; k++) {
+					assignments[k] = assignments[k+1];
+				}
+				assignments[assignments.length] = null;
+				return;
+			}
+		}
+		// delete assignment if there are no grades.
+		// if there are grades for the assignment, delete the 
+		// assignment only if "force" is specified
+		return;
 	}
 	
 
